a1881abe8d547bf0243c98a7f831b8c01e258d80
WebcrazyDylan <webcrazy@naver.com>
2021-04-16 21:10:34 -0700

    #14.6 Create Account part Two (13:39)

--- screens/CreateAccount.js
+++ screens/CreateAccount.js
@@ -1,10 +1,45 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { TextInput } from "react-native";
+import styled from "styled-components/native";
+import AuthButton from "../components/auth/AuthButton";
+import AuthLayout from "../components/auth/AuthLayout";
 
 export default function CreateAccount() {
   return (
-    <View>
-      <Text>CreateAccount</Text>
-    </View>
+    <AuthLayout>
+      <TextInput
+        placeholder="First Name"
+        placeholderTextColor="gray"
+        returnKeyType="next"
+        style={{ backgroundColor: "white", width: "100%" }}
+      />
+      <TextInput
+        placeholder="Last Name"
+        placeholderTextColor="gray"
+        returnKeyType="next"
+        style={{ backgroundColor: "white", width: "100%" }}
+      />
+      <TextInput
+        placeholder="Username"
+        placeholderTextColor="gray"
+        returnKeyType="next"
+        style={{ backgroundColor: "white", width: "100%" }}
+      />
+      <TextInput
+        placeholder="Email"
+        placeholderTextColor="gray"
+        keyboardType="email-address"
+        returnKeyType="next"
+        style={{ backgroundColor: "white", width: "100%" }}
+      />
+      <TextInput
+        placeholder="Password"
+        placeholderTextColor="gray"
+        secureTextEntry
+        returnKeyType="done"
+        style={{ backgroundColor: "white", width: "100%" }}
+      />
+      <AuthButton text="Create Account" disabled={true} onPress={() => null} />
+    </AuthLayout>
   );
 }
