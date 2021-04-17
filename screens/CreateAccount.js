7662c1ff1a5cfd2e83e43729239ab75ddf427941
WebcrazyDylan <webcrazy@naver.com>
2021-04-16 21:42:19 -0700

    #14.8 Natural Forms part Two (09:29)

--- screens/CreateAccount.js
+++ screens/CreateAccount.js
@@ -1,45 +1,77 @@
-import React from "react";
-import { TextInput } from "react-native";
+import React, { useRef } from "react";
+import { KeyboardAvoidingView, Platform, TextInput } from "react-native";
 import styled from "styled-components/native";
 import AuthButton from "../components/auth/AuthButton";
 import AuthLayout from "../components/auth/AuthLayout";
 
 export default function CreateAccount() {
+  const lastNameRef = useRef();
+  const usernameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  const onNext = (nextOne) => {
+    nextOne?.current?.focus();
+  };
+  const onDone = () => {
+    alert("done!");
+  };
   return (
     <AuthLayout>
-      <TextInput
-        placeholder="First Name"
-        placeholderTextColor="gray"
-        returnKeyType="next"
-        style={{ backgroundColor: "white", width: "100%" }}
-      />
-      <TextInput
-        placeholder="Last Name"
-        placeholderTextColor="gray"
-        returnKeyType="next"
-        style={{ backgroundColor: "white", width: "100%" }}
-      />
-      <TextInput
-        placeholder="Username"
-        placeholderTextColor="gray"
-        returnKeyType="next"
-        style={{ backgroundColor: "white", width: "100%" }}
-      />
-      <TextInput
-        placeholder="Email"
-        placeholderTextColor="gray"
-        keyboardType="email-address"
-        returnKeyType="next"
-        style={{ backgroundColor: "white", width: "100%" }}
-      />
-      <TextInput
-        placeholder="Password"
-        placeholderTextColor="gray"
-        secureTextEntry
-        returnKeyType="done"
-        style={{ backgroundColor: "white", width: "100%" }}
-      />
-      <AuthButton text="Create Account" disabled={true} onPress={() => null} />
+      <KeyboardAvoidingView
+        style={{
+          width: "100%"
+        }}
+        behavior="padding"
+        keyboardVerticalOffset={Platform.OS === "ios" ? 30 : 0}
+      >
+        <TextInput
+          placeholder="First Name"
+          placeholderTextColor="gray"
+          returnKeyType="next"
+          style={{ backgroundColor: "white", width: "100%" }}
+          onSubmitEditing={() => onNext(lastNameRef)}
+        />
+        <TextInput
+          ref={lastNameRef}
+          placeholder="Last Name"
+          placeholderTextColor="gray"
+          returnKeyType="next"
+          style={{ backgroundColor: "white", width: "100%" }}
+          onSubmitEditing={() => onNext(usernameRef)}
+        />
+        <TextInput
+          ref={usernameRef}
+          placeholder="Username"
+          placeholderTextColor="gray"
+          returnKeyType="next"
+          style={{ backgroundColor: "white", width: "100%" }}
+          onSubmitEditing={() => onNext(emailRef)}
+        />
+        <TextInput
+          ref={emailRef}
+          placeholder="Email"
+          placeholderTextColor="gray"
+          keyboardType="email-address"
+          returnKeyType="next"
+          style={{ backgroundColor: "white", width: "100%" }}
+          onSubmitEditing={() => onNext(passwordRef)}
+        />
+        <TextInput
+          ref={passwordRef}
+          placeholder="Password"
+          placeholderTextColor="gray"
+          secureTextEntry
+          returnKeyType="done"
+          style={{ backgroundColor: "white", width: "100%" }}
+          onSubmitEditing={onDone}
+        />
+        <AuthButton
+          text="Create Account"
+          disabled={true}
+          onPress={() => null}
+        />
+      </KeyboardAvoidingView>
     </AuthLayout>
   );
 }
