00edeeb1c4d1628f1d080cef602f7d8400320ba1
WebcrazyDylan <webcrazy@naver.com>
2021-04-21 16:00:33 -0700

    #17.3 Select Photo part Two (12:05)

--- screens/SelectPhoto.js
+++ screens/SelectPhoto.js
@@ -1,10 +1,53 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import * as MediaLibrary from "expo-media-library";
+import styled from "styled-components/native";
+
+const Container = styled.View`
+  flex: 1;
+  background-color: black;
+`;
+
+const Top = styled.View`
+  flex: 1;
+  background-color: black;
+`;
+
+const Bottom = styled.View`
+  flex: 1;
+  background-color: black;
+`;
 
 export default function SelectPhoto() {
+  const [ok, setOk] = useState(false);
+  const [photos, setPhotos] = useState([]);
+  const getPhotos = async () => {
+    if (ok) {
+      const { assets: photos } = await MediaLibrary.getAssetsAsync();
+      setPhotos(photos);
+    }
+  };
+  const getPermissions = async () => {
+    const {
+      accessPrivileges,
+      canAskAgain
+    } = await MediaLibrary.getPermissionsAsync();
+    if (accessPrivileges === "none" && canAskAgain) {
+      const { accessPrivileges } = await MediaLibrary.requestPermissionsAsync();
+      if (accessPrivileges !== "none") {
+        setOk(true);
+      }
+    } else if (accessPrivileges !== "none") {
+      setOk(true);
+    }
+  };
+  useEffect(() => {
+    getPermissions();
+    getPhotos();
+  }, []);
   return (
-    <View>
-      <Text>SelectPhoto</Text>
-    </View>
+    <Container>
+      <Top />
+      <Bottom></Bottom>
+    </Container>
   );
 }
