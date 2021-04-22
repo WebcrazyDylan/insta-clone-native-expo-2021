c89d81e76e483411662ac67b94b5788565b42e08
WebcrazyDylan <webcrazy@naver.com>
2021-04-22 16:11:51 -0700

    #17.15 Upload Screen part Four (10:42)

--- screens/SelectPhoto.js
+++ screens/SelectPhoto.js
@@ -66,7 +66,13 @@ export default function SelectPhoto({ navigation }) {
     }
   };
   const HeaderRight = () => (
-    <TouchableOpacity>
+    <TouchableOpacity
+      onPress={() =>
+        navigation.navigate("UploadForm", {
+          file: chosenPhoto
+        })
+      }
+    >
       <HeaderRightText>Next</HeaderRightText>
     </TouchableOpacity>
   );
@@ -77,7 +83,7 @@ export default function SelectPhoto({ navigation }) {
     navigation.setOptions({
       headerRight: HeaderRight
     });
-  }, []);
+  }, [chosenPhoto]);
   const numColumns = 4;
   const { width } = useWindowDimensions();
   const choosePhoto = (uri) => {
@@ -100,7 +106,7 @@ export default function SelectPhoto({ navigation }) {
   );
   return (
     <Container>
-      <StatusBar />
+      <StatusBar hidden={false} />
       <Top>
         {chosenPhoto !== "" ? (
           <Image
---
+++ screens/UploadForm.js
@@ -0,0 +1,116 @@
+import { gql, useMutation } from "@apollo/client";
+import { ReactNativeFile } from "apollo-upload-client";
+import React, { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { ActivityIndicator, Text, View } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import styled from "styled-components/native";
+import { colors } from "../colors";
+import DismissKeyboard from "../components/DismissKeyboard";
+import { FEED_PHOTO } from "../fragments";
+
+const UPLOAD_PHOTO_MUTATION = gql`
+  mutation uploadPhoto($file: Upload!, $caption: String) {
+    uploadPhoto(file: $file, caption: $caption) {
+      ...FeedPhoto
+    }
+  }
+  ${FEED_PHOTO}
+`;
+
+const Container = styled.View`
+  flex: 1;
+  background-color: black;
+  padding: 0px 50px;
+`;
+const Photo = styled.Image`
+  height: 350px;
+`;
+const CaptionContainer = styled.View`
+  margin-top: 30px;
+`;
+const Caption = styled.TextInput`
+  background-color: white;
+  color: black;
+  padding: 10px 20px;
+  border-radius: 100px;
+`;
+
+const HeaderRightText = styled.Text`
+  color: ${colors.blue};
+  font-size: 16px;
+  font-weight: 600;
+  margin-right: 7px;
+`;
+
+export default function UploadForm({ route, navigation }) {
+  const updateUploadPhoto = (cache, result) => {
+    const {
+      data: { uploadPhoto }
+    } = result;
+    if (uploadPhoto.id) {
+      cache.modify({
+        id: "ROOT_QUERY",
+        fields: {
+          seeFeed(prev) {
+            return [uploadPhoto, ...prev];
+          }
+        }
+      });
+      navigation.navigate("Tabs");
+    }
+  };
+  const [uploadPhotoMutation, { loading }] = useMutation(
+    UPLOAD_PHOTO_MUTATION,
+    {
+      update: updateUploadPhoto
+    }
+  );
+  const HeaderRight = () => (
+    <TouchableOpacity onPress={handleSubmit(onValid)}>
+      <HeaderRightText>Next</HeaderRightText>
+    </TouchableOpacity>
+  );
+  const HeaderRightLoading = () => (
+    <ActivityIndicator size="small" color="white" style={{ marginRight: 10 }} />
+  );
+  const { register, handleSubmit, setValue } = useForm();
+  useEffect(() => {
+    register("caption");
+  }, [register]);
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: loading ? HeaderRightLoading : HeaderRight,
+      ...(loading && { headerLeft: () => null })
+    });
+  }, [loading]);
+  const onValid = ({ caption }) => {
+    const file = new ReactNativeFile({
+      uri: route.params.file,
+      name: `1.jpg`,
+      type: "image/jpeg"
+    });
+    uploadPhotoMutation({
+      variables: {
+        caption,
+        file
+      }
+    });
+  };
+  return (
+    <DismissKeyboard>
+      <Container>
+        <Photo resizeMode="contain" source={{ uri: route.params.file }} />
+        <CaptionContainer>
+          <Caption
+            returnKeyType="done"
+            placeholder="Write a caption..."
+            placeholderTextColor="rgba(0, 0, 0, 0.5)"
+            onSubmitEditing={handleSubmit(onValid)}
+            onChangeText={(text) => setValue("caption", text)}
+          />
+        </CaptionContainer>
+      </Container>
+    </DismissKeyboard>
+  );
+}
