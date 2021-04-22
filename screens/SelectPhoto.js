78388baafb1aede401ed13aa2bd581837b056577
WebcrazyDylan <webcrazy@naver.com>
2021-04-21 21:14:47 -0700

    #17.8 Take Photo part Three (08:28)

--- screens/SelectPhoto.js
+++ screens/SelectPhoto.js
@@ -1,6 +1,15 @@
 import React, { useEffect, useState } from "react";
+import { Ionicons } from "@expo/vector-icons";
 import * as MediaLibrary from "expo-media-library";
 import styled from "styled-components/native";
+import {
+  FlatList,
+  Image,
+  StatusBar,
+  TouchableOpacity,
+  useWindowDimensions
+} from "react-native";
+import { colors } from "../colors";
 
 const Container = styled.View`
   flex: 1;
@@ -17,14 +26,28 @@ const Bottom = styled.View`
   background-color: black;
 `;
 
-export default function SelectPhoto() {
+const ImageContainer = styled.TouchableOpacity``;
+const IconContainer = styled.View`
+  position: absolute;
+  bottom: 5px;
+  right: 0px;
+`;
+
+const HeaderRightText = styled.Text`
+  color: ${colors.blue};
+  font-size: 16px;
+  font-weight: 600;
+  margin-right: 7px;
+`;
+
+export default function SelectPhoto({ navigation }) {
   const [ok, setOk] = useState(false);
   const [photos, setPhotos] = useState([]);
+  const [chosenPhoto, setChosenPhoto] = useState("");
   const getPhotos = async () => {
-    if (ok) {
-      const { assets: photos } = await MediaLibrary.getAssetsAsync();
-      setPhotos(photos);
-    }
+    const { assets: photos } = await MediaLibrary.getAssetsAsync();
+    setPhotos(photos);
+    setChosenPhoto(photos[0]?.uri);
   };
   const getPermissions = async () => {
     const {
@@ -35,19 +58,65 @@ export default function SelectPhoto() {
       const { accessPrivileges } = await MediaLibrary.requestPermissionsAsync();
       if (accessPrivileges !== "none") {
         setOk(true);
+        getPhotos();
       }
     } else if (accessPrivileges !== "none") {
       setOk(true);
+      getPhotos();
     }
   };
+  const HeaderRight = () => (
+    <TouchableOpacity>
+      <HeaderRightText>Next</HeaderRightText>
+    </TouchableOpacity>
+  );
   useEffect(() => {
     getPermissions();
-    getPhotos();
   }, []);
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: HeaderRight
+    });
+  }, []);
+  const numColumns = 4;
+  const { width } = useWindowDimensions();
+  const choosePhoto = (uri) => {
+    setChosenPhoto(uri);
+  };
+  const renderItem = ({ item: photo }) => (
+    <ImageContainer onPress={() => choosePhoto(photo.uri)}>
+      <Image
+        source={{ uri: photo.uri }}
+        style={{ width: width / numColumns, height: 100 }}
+      />
+      <IconContainer>
+        <Ionicons
+          name="checkmark-circle"
+          size={18}
+          color={photo.uri === chosenPhoto ? colors.blue : "white"}
+        />
+      </IconContainer>
+    </ImageContainer>
+  );
   return (
     <Container>
-      <Top />
-      <Bottom></Bottom>
+      <StatusBar />
+      <Top>
+        {chosenPhoto !== "" ? (
+          <Image
+            source={{ uri: chosenPhoto }}
+            style={{ width, height: "100%" }}
+          />
+        ) : null}
+      </Top>
+      <Bottom>
+        <FlatList
+          data={photos}
+          numColumns={numColumns}
+          keyExtractor={(photo) => photo.id}
+          renderItem={renderItem}
+        />
+      </Bottom>
     </Container>
   );
 }
--- screens/TakePhoto.js
+++ screens/TakePhoto.js
@@ -1,10 +1,138 @@
-import React from "react";
-import { Text, View } from "react-native";
+import { Camera } from "expo-camera";
+import React, { useEffect, useState } from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { StatusBar, TouchableOpacity } from "react-native";
+import Slider from "@react-native-community/slider";
+import styled from "styled-components/native";
 
-export default function TakePhoto() {
+const Container = styled.View`
+  flex: 1;
+  background-color: black;
+`;
+
+const Actions = styled.View`
+  flex: 0.35;
+  padding: 0px 50px;
+  align-items: center;
+  justify-content: space-around;
+`;
+
+const ButtonsContainer = styled.View`
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const TakePhotoBtn = styled.TouchableOpacity`
+  width: 100px;
+  height: 100px;
+  background-color: rgba(255, 255, 255, 0.5);
+  border: 2px solid rgba(255, 255, 255, 0.8);
+  border-radius: 50px;
+`;
+
+const SliderContainer = styled.View``;
+const ActionsContainer = styled.View`
+  flex-direction: row;
+`;
+
+const CloseButton = styled.TouchableOpacity`
+  position: absolute;
+  top: 20px;
+  left: 20px;
+`;
+
+export default function TakePhoto({ navigation }) {
+  const [ok, setOk] = useState(false);
+  const [flashMode, setFlashMode] = useState(Camera.Constants.FlashMode.off);
+  const [zoom, setZoom] = useState(0);
+  const [cameraType, setCameraType] = useState(Camera.Constants.Type.front);
+  const getPermissions = async () => {
+    const { granted } = await Camera.requestPermissionsAsync();
+    setOk(granted);
+  };
+  useEffect(() => {
+    getPermissions();
+  }, []);
+  const onCameraSwitch = () => {
+    if (cameraType === Camera.Constants.Type.front) {
+      setCameraType(Camera.Constants.Type.back);
+    } else {
+      setCameraType(Camera.Constants.Type.front);
+    }
+  };
+  const onZoomValueChange = (e) => {
+    setZoom(e);
+  };
+  const onFlashChange = () => {
+    if (flashMode === Camera.Constants.FlashMode.off) {
+      setFlashMode(Camera.Constants.FlashMode.on);
+    } else if (flashMode === Camera.Constants.FlashMode.on) {
+      setFlashMode(Camera.Constants.FlashMode.auto);
+    } else if (flashMode === Camera.Constants.FlashMode.auto) {
+      setFlashMode(Camera.Constants.FlashMode.off);
+    }
+  };
   return (
-    <View>
-      <Text>TakePhoto</Text>
-    </View>
+    <Container>
+      <StatusBar hidden={true} />
+      <Camera
+        type={cameraType}
+        style={{ flex: 1 }}
+        zoom={zoom}
+        flashMode={flashMode}
+      >
+        <CloseButton onPress={() => navigation.navigate("Tabs")}>
+          <Ionicons name="close" color="white" size={30} />
+        </CloseButton>
+      </Camera>
+      <Actions>
+        <SliderContainer>
+          <Slider
+            style={{ width: 200, height: 20 }}
+            minimumValue={0}
+            maximumValue={1}
+            minimumTrackTintColor="#FFFFFF"
+            maximumTrackTintColor="rgba(255, 255, 255, 0.5)"
+            onValueChange={onZoomValueChange}
+          />
+        </SliderContainer>
+        <ButtonsContainer>
+          <TakePhotoBtn />
+          <ActionsContainer>
+            <TouchableOpacity
+              onPress={onFlashChange}
+              style={{ marginRight: 30 }}
+            >
+              <Ionicons
+                size={30}
+                color="white"
+                name={
+                  flashMode === Camera.Constants.FlashMode.off
+                    ? "flash-off"
+                    : flashMode === Camera.Constants.FlashMode.on
+                    ? "flash"
+                    : flashMode === Camera.Constants.FlashMode.auto
+                    ? "eye"
+                    : ""
+                }
+              />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={onCameraSwitch}>
+              <Ionicons
+                size={30}
+                color="white"
+                name={
+                  cameraType === Camera.Constants.Type.front
+                    ? "camera-reverse"
+                    : "camera"
+                }
+              />
+            </TouchableOpacity>
+          </ActionsContainer>
+        </ButtonsContainer>
+      </Actions>
+    </Container>
   );
 }
