55d4f09a9ce0cdae1e0f75f1f497f51376144d5e
WebcrazyDylan <webcrazy@naver.com>
2021-04-22 18:42:40 -0700

    #18.2 Rooms Screen part Two (11:49)

---
+++ navigators/MessagesNav.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { createStackNavigator } from "@react-navigation/stack";
+import Room from "../screens/Room";
+import Rooms from "../screens/Rooms";
+
+const Stack = createStackNavigator();
+
+export default function MessagesNav() {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerTintColor: "white",
+        headerBackTitleVisible: false,
+        headerStyle: {
+          backgroundColor: "black"
+        },
+        headerBackImage: ({ tintColor }) => (
+          <Ionicons color={tintColor} name="chevron-down" size={28} />
+        )
+      }}
+    >
+      <Stack.Screen name="Rooms" component={Rooms} />
+      <Stack.Screen name="Room" component={Room} />
+    </Stack.Navigator>
+  );
+}
